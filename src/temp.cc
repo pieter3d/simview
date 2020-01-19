2b57f5de03a538973e4f4b3ca6add78b452cd538
Pieter Kapsenberg <pieter.kapsenberg@gmail.com>
2020-01-18 23:40:54 -0800

    Initial commit with basic cmake build infra stuff in place

---
+++ src/temp.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+ 
+#include "absl/strings/str_format.h"
+
+void Temp() {
+  // Just placeholder coder to test cmake build setup.
+  std::cout << absl::StrFormat("blah 0x%x\n", 25);
+}
