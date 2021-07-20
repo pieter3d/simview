e2166c81a988b27c93dca7176c28fd836295b334
Pieter Kapsenberg <pieter.kapsenberg@gmail.com>
2021-07-19 22:56:06 -0700

    Simple pane drawing and resizing

--- src/ui.cc
+++ src/ui.cc
@@ -5,17 +5,20 @@
 namespace sv {
 
 UI::UI() {
-  int term_w, term_h;
-  getmaxyx(stdscr, term_h, term_w);
-  // Default splits on startup.
-  src_pos_x_ = term_w * 30 / 100;
-  wave_pos_y_ = term_h * 50 / 100;
-
   // Init ncurses
   initscr();
   raw();
   keypad(stdscr, true);
   noecho();
+  curs_set(0); // Hide cursor
+
+  // Default splits on startup.
+  getmaxyx(stdscr, term_h_, term_w_);
+  src_pos_x_ = term_w_ * 30 / 100;
+  wave_pos_y_ = term_h_ * 50 / 100;
+
+  // Initial render
+  DrawPanes();
 }
 
 UI::~UI() {
@@ -24,8 +27,60 @@ UI::~UI() {
 }
 
 void UI::EventLoop() {
-  // TODO: just get a character and return for now.
-  getch();
+  int ch = 0;
+  while (1) {
+    ch = getch();
+    if (ch == KEY_RESIZE) {
+      float x = (float)src_pos_x_ / term_w_;
+      float y = (float)wave_pos_y_ / term_h_;
+      getmaxyx(stdscr, term_h_, term_w_);
+      src_pos_x_ = (int)(term_w_ * x);
+      wave_pos_y_ = (int)(term_h_ * y);
+      DrawPanes();
+
+      continue;
+    }
+    switch (ch) {
+      case 0x221: // ctrl-left
+        if (src_pos_x_ > 5) {
+          src_pos_x_--;
+          DrawPanes();
+        }
+        break;
+      case 0x230: // ctrl-right
+        if (src_pos_x_ < term_w_ - 5) {
+          src_pos_x_++;
+          DrawPanes();
+        }
+        break;
+      case 0x236: // ctrl-up
+        if (wave_pos_y_ > 5) {
+          wave_pos_y_--;
+          DrawPanes();
+        }
+        break;
+      case 0x20d: // ctrl-down
+        if (wave_pos_y_ < term_h_ - 5) {
+          wave_pos_y_++;
+          DrawPanes();
+        }
+        break;
+    }
+    // For now:
+    if (ch == 'q') break;
+  }
+}
+
+void UI::DrawPanes() {
+  clear();
+  for (int row = 0; row < wave_pos_y_; ++row) {
+    mvaddch(row, src_pos_x_, ACS_VLINE);
+  }
+  move(wave_pos_y_, 0);
+  for (int col = 0; col < term_w_; ++col) {
+    addch(col == src_pos_x_ ? ACS_BTEE : ACS_HLINE);
+  }
+  refresh();
 }
 
 } // namespace sv
--- src/ui.h
+++ src/ui.h
@@ -11,8 +11,12 @@ class UI {
   void EventLoop();
 
  private:
+  void DrawPanes();
+
   int wave_pos_y_;
   int src_pos_x_;
+  int term_w_;
+  int term_h_;
 };
 } // namespace sv
 #endif // _SRC_UI_H_
