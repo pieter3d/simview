b8e4296b7e9ee36dc15aa3a12cec782a8f0589b5
Pieter Kapsenberg <pieter.kapsenberg@gmail.com>
2021-08-08 00:56:37 -0700

    removing pointless file

--- src/temp.cc
+++
@@ -1,8 +0,0 @@
-#include <iostream>
- 
-#include "absl/strings/str_format.h"
-
-void Temp() {
-  // Just placeholder coder to test cmake build setup.
-  std::cout << absl::StrFormat("blah 0x%x\n", 25);
-}
