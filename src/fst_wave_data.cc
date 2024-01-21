827d492abf16ac55552ab1914113abdc3a106e04
Pieter Kapsenberg <pieter.kapsenberg@gmail.com>
2024-01-21 10:08:36 -0800

    removed debug print

--- src/fst_wave_data.cc
+++ src/fst_wave_data.cc
@@ -67,7 +67,6 @@ void FstWaveData::ReadScopes() {
       signal.id = h->u.var.handle;
       signal.width = h->u.var.length;
       signal.name = ParseSignalLsb(name, &signal.lsb);
-      printf("signal: %s\n", name.c_str());
       switch (h->u.var.direction) {
       case FST_VD_IMPLICIT: signal.direction = Signal::kInternal; break;
       case FST_VD_INOUT: signal.direction = Signal::kInout; break;
